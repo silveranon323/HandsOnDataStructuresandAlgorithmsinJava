293ae4005eb08c6204542c4a0ef04526c27faab4
Hem Kishore Pradhan <101815577+silveranon323@users.noreply.github.com>
2025-08-03 00:26:58 +0530

    Added Search String Program-Uses bufferreader|line.contains()

--- DSAJAVA/src/Strings/SearchString.java
+++ DSAJAVA/src/Strings/SearchString.java
@@ -1,7 +1,22 @@
 package Strings;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class SearchString {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader= new BufferedReader(new FileReader("/Users/hemkishorepradhan/Desktop/HandsOnDataStructuresandAlgorithmsinJava/DSAJAVA/src/Strings/document.txt"));
+        String line;
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the string you want to search: ");
+        String searchString=sc.nextLine();
+        while((line=reader.readLine())!=null){
+            if(line.contains(searchString)){
+                System.out.println("String found at index "+line.indexOf(searchString));
+            }
+        }
     }
 }
